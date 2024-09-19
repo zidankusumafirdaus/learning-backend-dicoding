a743274e2dcf39d8ab1253ff79d60428896fb47d
M Zidan Kusuma Firdaus <zidankusumafirdaus.school@gmail.com>
2024-09-19 11:10:11 +0700

    third commit

--- routes.js
+++ routes.js
@@ -1,42 +1,15 @@
 const routes = [
   {
     method: "GET",
-    path: "/",
-    handler: (request, h) => {
-      return "Homepage";
-    },
-  },
-  {
-    method: "*",
-    path: "/",
-    handler: (request, h) => {
-      return "Halaman tidak dapat diakses dengan method tersebut";
-    },
-  },
-  {
-    method: "GET",
-    path: "/about",
-    handler: (request, h) => {
-      return "About page";
-    },
-  },
-  {
-    method: "*",
-    path: "/about",
-    handler: (request, h) => {
-      return "Halaman tidak dapat diakses dengan method";
-    },
-  },
-  {
-    method: "GET",
     path: "/hello/{name?}",
-    handler: (request, h) => {},
-  },
-  {
-    method: "*",
-    path: "/{any*}",
     handler: (request, h) => {
-      return "Halaman tidak ditemukan";
+      const { name = "stranger" } = request.params;
+      const { lang } = request.query;
+
+      if (lang === "id") {
+        return `Hai, ${name}!`;
+      }
+      return `Hello, ${name}!`;
     },
   },
 ];
